74bf453944a347263c72b325da68a90c367d8acc
xudongdev <me@xudong.dev>
2020-08-09 02:23:52 +0800

    feat: redis machine id

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
+import Redis from "ioredis";
+
 export interface SnowflakeIdGeneratorOptions {
   timestampBits?: number | bigint;
-  machineBits?: number | bigint;
+  machineIdBits?: number | bigint;
 
   epoch?: number | bigint;
   machineId?: number | bigint;
@@ -23,7 +25,7 @@ export class SnowflakeIdGenerator {
 
   constructor(options?: SnowflakeIdGeneratorOptions) {
     this.timestampBits = BigInt(options?.timestampBits || 41n);
-    this.machineIdBits = BigInt(options?.machineBits || 10n);
+    this.machineIdBits = BigInt(options?.machineIdBits || 10n);
     this.sequenceBits = 64n - this.timestampBits - this.machineIdBits;
 
     this.timestampShift = this.sequenceBits + this.machineIdBits;
@@ -34,8 +36,9 @@ export class SnowflakeIdGenerator {
     this.epoch = BigInt(options?.epoch || new Date("2020-01-01").getTime());
 
     this.machineId = BigInt(
-      options?.machineId ||
-        Math.floor(Math.random() * 2 ** Number(this.machineIdBits))
+      ["number", "bigint"].includes(typeof options?.machineId)
+        ? options.machineId
+        : Math.floor(Math.random() * 2 ** Number(this.machineIdBits))
     );
   }
 
@@ -50,8 +53,49 @@ export class SnowflakeIdGenerator {
   }
 
   private static instance: SnowflakeIdGenerator;
+  private static redis: Redis.Redis;
+  private static timer: NodeJS.Timeout;
 
-  static set(options: SnowflakeIdGeneratorOptions): void {
+  static async set(
+    options?: SnowflakeIdGeneratorOptions,
+    redis?: { options: string | Redis.RedisOptions }
+  ): Promise<void> {
+    clearInterval(this.timer);
+
+    if (!["number", "bigint"].includes(typeof options.machineId) && redis) {
+      this.redis = new Redis(redis?.options as string);
+
+      const machineIdMask = 2n ** BigInt(options?.machineIdBits || 10n);
+
+      let machineId = 0n;
+      while (!["number", "bigint"].includes(typeof options.machineId)) {
+        if (
+          await this.redis.set(
+            `snowflake-id-generator:${machineId}`,
+            "",
+            "EX",
+            180,
+            "NX"
+          )
+        ) {
+          this.timer = setInterval(
+            () =>
+              this.redis.set(
+                `snowflake-id-generator:${machineId}`,
+                "",
+                "EX",
+                180
+              ),
+            60000
+          );
+
+          options.machineId = machineId;
+        } else {
+          machineId = ++machineId % machineIdMask;
+        }
+      }
+    }
+
     this.instance = new this(options);
   }
 
